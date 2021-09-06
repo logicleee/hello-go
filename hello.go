a3038bfe494259af14b1fb0108c3bcb1fd8481a9
lee g <logicleeee+github@gmail.com>
2021-09-06 13:02:12 -0500

    imported first package from remote

--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,wolleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hellow Go"))
 }
